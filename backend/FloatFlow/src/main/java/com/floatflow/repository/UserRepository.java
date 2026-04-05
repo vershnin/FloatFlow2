c939d298a30778916eda054112592c10acc8b9b6
vershnin <richardorinda@gmail.com>
2026-04-05 23:17:07 +0300

    feat: add custom queries to UserRepository for fetching users with branch details

--- backend/FloatFlow/src/main/java/com/floatflow/repository/UserRepository.java
+++ backend/FloatFlow/src/main/java/com/floatflow/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.floatflow.repository;
 
 import com.floatflow.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -12,6 +15,13 @@ import java.util.Optional;
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.branch")
+    List<User> findAll();
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.branch WHERE u.id = :id")
+    Optional<User> findById(Long id);
+
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
